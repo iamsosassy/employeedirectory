da6cb5bc382d43a8b183848a855cce84094e9616
iamsosassy <shirleyrivera70@gmail.com>
2020-04-29 12:11:12 -0400

    initial commit with files

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DataInfo from "./DataArea";
+import "../styles/Main.css";
+
+function Main() {
+    return (
+        <>
+            <DataInfo />
+        </>
+    );
+};
+
+export default Main;
